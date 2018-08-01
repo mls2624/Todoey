d298bdea696c0c92ba749d365ebcc4b2df7e1477
Marwan Mohsen <mls2624@gmail.com>
2018-08-01 10:06:06 +0200

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Marwan Mohsen on 8/1/18.
+//  Copyright © 2018 M3WE. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
